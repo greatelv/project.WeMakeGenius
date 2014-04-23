a5ef901424700d499f7bb28f8adb1ec3c689b823
tkjeon <wjswjs2@mhand.net>
2014-04-23 16:42:47 +0900

    - home page 플레이 버튼 핸들링

--- WebContent/assets/js/navi.js
+++ WebContent/assets/js/navi.js
@@ -1,10 +1,11 @@
 //메인 초기화
 //페이지간의 전환
-$(function gnbClickHandler() {
+$(function GnbHandler() {
 
 	$('.page').hide();
 	$('#page_home').show();
 
+	//GNB 핸들러
 	$('.page-locater').click(function(){
 		var targetRef = $(this).attr('ref');
 
@@ -19,5 +20,15 @@ $(function gnbClickHandler() {
 
 	//순위 정보 Marquee
 	$("#rank-scroll > ul").liScroll({travelocity: 0.13});
+
+});
+
+//Home 페이지 핸들러
+$(function HomeHandler() {
+
+	//Home Page 게임 플레이 버튼 핸들러
+	$('#hello button').click(function(){
+		$('.page-locater[ref="play"]').trigger('click');
+	});
 
 });
