f3e03984ac7c9a8e00564bd821fe1d0cb51d8a24
Lee Geun Jae <lgj9172@gmail.com>
2014-05-08 00:04:44 +0900

    회원가입 json 미완성

---
+++ WebContent/assets/js/join.js
@@ -0,0 +1,26 @@
+﻿
+$(document).ready(function(){
+	$('#join').click(function(){
+			$postid 		= document.getElementById("input-id").value;
+			$postname		= document.getElementById("input-name").value;
+			$postavatar		= document.getElementById("input-pass").value;
+			var json = {"AVATAR":$postavatar,"NAME":$postname,"ID":$postid};
+			console.log(json);
+			$.ajax({
+				url		:	"join.jsp",
+				type	:	"POST",
+				data	:	json,
+				datatype:	"json",
+				
+				success	: function(datapost){
+					alert(datapost);
+				},
+				error	: function(){
+					console.log('error from post');
+				},
+				complete: function(){
+					console.log('complete from post');
+				}
+			});
+		});
+});
