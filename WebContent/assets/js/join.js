0596e1bac9faf1d44b5e793c4398af55488749a5
lgj9172 <lgj9172@gmail.com>
2014-05-14 20:20:53 +0900

    프론트엔드에서 가입시 아바타 제거

--- WebContent/assets/js/join.js
+++ WebContent/assets/js/join.js
@@ -3,9 +3,8 @@ $(document).ready(function(){
 	$('#join').click(function(){
 			var postid = document.getElementById("input-register-id").value;
 			var postname = document.getElementById("input-register-name").value;
-			var postavatar = document.getElementById("input-register-avatar").value;
 			alert(postid +" 님 회원가입을 시도중입니다.");
-			var json = {"AVATAR":postavatar,"NAME":postname,"ID":postid};
+			var json = {"NAME":postname,"ID":postid};
 			console.log(json);
 			$.ajax({
 				url		:	"jsp/join.jsp",
@@ -13,8 +12,8 @@ $(document).ready(function(){
 				data	:	json,
 				datatype:	"json",
 				
-				success	: function(datapost){
-					alert(datapost);
+				success	: function(result){
+					console.log(result);
 				},
 				error	: function(){
 					console.log('error from post');
