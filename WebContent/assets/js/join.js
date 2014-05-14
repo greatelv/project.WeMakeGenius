534702bc4959c74309b9f8bd9f74faeebcb9591b
lgj9172 <lgj9172@gmail.com>
2014-05-14 18:29:19 +0900

    프론트엔드에서 회원가입 제거

--- WebContent/assets/js/join.js
+++ WebContent/assets/js/join.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 	$('#join').click(function(){
 			var postid = document.getElementById("input-register-id").value;
 			var postname = document.getElementById("input-register-name").value;
-			var postavatar = document.getElementById("input-register-pass").value;
+			var postavatar = document.getElementById("input-register-avatar").value;
 			alert(postid +" 님 회원가입을 시도중입니다.");
 			var json = {"AVATAR":postavatar,"NAME":postname,"ID":postid};
 			console.log(json);
