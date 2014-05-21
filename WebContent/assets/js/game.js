d5ee5ca76605829a8e9818ab21ff4dcb0592fcc2
tkjeon <wjswjs2@gmail.com>
2014-05-21 20:18:36 +0900

    - 게임 클리어 메소드 작성 - 클로져 패턴으로 게임 공통 처리모듈 개발 - 핸들러에 게임 클리어 메소드 콜

--- WebContent/assets/js/game.js
+++ WebContent/assets/js/game.js
@@ -1,4 +1,4 @@
-//레디고 출력
+//게임 공통 요소 처리 모듈
 var game = function(){
 
 	//필요한 엘리먼트 캐싱
@@ -6,8 +6,13 @@ var game = function(){
 		This : 			$('#page_games'),
 		score : 		$('#score'),
 		startMessge : 	$('#start_message'),
-		gameHeader : 	$('.game-info-header')
+		gameHeader : 	$('.game-info-header'),
+		timer : 		$('#timer'),
+		progess : 		$('#timeline-progress')
 	};
+
+	//문제 풀이시 획득 포인트
+	var acqPoint = 1000;
 
 	var point = 0;
 	var combo = 0;
@@ -16,40 +21,39 @@ var game = function(){
 		elem.score.text(point+' Point');
 	};
 
-
+	//setTimer 객체들
+	var setTimerArray = [];
 
 	return {
-		readyGo : function(){
+		readyGo : function(){	//레디고 출력
 			$('.page').hide();
 		    $('#page_games').show();
 		    $('#rank-scroll').hide();
 		    
-		    setTimeout(function(){
+		    setTimerArray.push(setTimeout(function(){
 		    	$("#start_message").html("<img src='assets/img/game/img_ready.png'></img>");
 		    	
-		    	setTimeout(function(){
+		    	setTimerArray.push(setTimeout(function(){
 		    		$("#start_message").html("<img src='assets/img/game/img_go.png'></img>");
 		    		
-		    		setTimeout(function(){
+		    		setTimerArray(setTimeout(function(){
 		        		$("#start_message").empty();
 						$('.game-info-header').show();
 
 		        		game.runTimer();
 
-		        	}, 1000);
+		        	}, 1000));
 		    		
-		    	}, 1500);
+		    	}, 1500));
 		    	
-		    }, 500);
+		    }, 500));
 		},
-		runTimer : function(){
+		runTimer : function(){ //타이머 출력
 			var limitSec = 10;
 			var duration = 1000 * limitSec;
 
-			var progressElem = $('#timeline-progress');
-			var timerElem = $('#timer');
-
-			progressElem.stop().width(0).animate({
+			
+			elem.progess.stop().width(0).animate({
 				width: '600px',
 			}, {
 				duration: duration,
@@ -60,13 +64,34 @@ var game = function(){
 					currentTime = Math.round((now * duration) / 600);
 					var nRemainTime =  parseInt(currentTime/1000);
 					var curSec = limitSec - nRemainTime;
-					timerElem.text(curSec);
+					elem.timer.text(curSec);
 					//_elem.current.text(nRemainTime.toString());
 				},
 				complete: function(){
 					console.log('complete!!');
 				}
 
+			});
+		},
+		solve : function(isCorrect){
+			if(isCorrect){
+				point = point + acqPoint;
+			}else{
+				point = point - acqPoint;
+			}
+			sycPoint();
+		},
+		clearGame : function(){
+			combo = 0;
+			point = 0;
+			sycPoint();
+
+			elem.gameHeader.hide();
+			elem.progess.stop().width(0)
+			_elem.timer.text(10);
+
+			$.each(setTimerArray, function(idx, item){
+				item.clearTimeout();
 			});
 		}
 	}
