156468e2f03286066040cfef6ea6b24b8b17b278
tkjeon <wjswjs2@gmail.com>
2014-05-21 17:03:01 +0900

    - logout 기능 적용

--- WebContent/assets/js/header.js
+++ WebContent/assets/js/header.js
@@ -14,7 +14,7 @@ var onSession = function(){
 		navElem.find('li').eq(3).hide();
 		navElem.find('li').eq(4).hide();
 
-		navElem.append('<li class="session-on">'+id+'님 환영합니다. <span class="glyphicon glyphicon-log-out" id="logout" alt="Logout" data-toggle="modal" data-target=".logout-modal"></span></li>');
+		navElem.append('<li class="session-on">'+name+'님 환영합니다. <span class="glyphicon glyphicon-log-out" id="logout" alt="Logout" data-toggle="modal" data-target=".logout-modal"></span></li>');
 	}
 };
 
