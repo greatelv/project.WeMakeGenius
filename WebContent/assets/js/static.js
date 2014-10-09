0d113da483ce6a8fda1919193a8e9031e74d2bf8
이근재 <lgj9172@gmail.com>
2014-10-09 22:58:57 +0900

    이 업데이트에서는 다음과 같은 변경사항이 있었습니다. 1. 새로운 게임에 대한 버튼이 생성되었습니다. 2. 새로운 게임에 대한 설명이 추가되었습니다.

--- WebContent/assets/js/static.js
+++ WebContent/assets/js/static.js
@@ -48,5 +48,17 @@ var gameMeta = [
 		desc : '그림에 나온 아이템의 개수를 기억한 후 정답을 맞추세요!',
 		guideColor : '#F4B284'
 	},
+	{
+		id : 'g3',
+		title : '색깔 순서 맞추기',
+		desc : '색깔이 나오는 순서를 보고 순서를 맞추세요!',
+		guideColor : '#2e75b6'
+	},
+	{
+		id : 'g4',
+		title : '짝 없는 그림 찾기',
+		desc : '짝이 없는 하나의 그림을 골라주세요!',
+		guideColor : '#ffd966'
+	}
 ];
 
